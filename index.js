bd2b2f734b0cc5aa7cbeb9273b41f46a00178a8a
Rafaella <rafaella.bergamo@plasutil.com.br>
2021-12-13 09:45:20 -0300

    Conectando ao banco

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(
@@ -9,4 +10,14 @@ app.use(
 
 app.use(express.json())
 
-app.listen(8000)
+app.get('/', (req, res) => {
+  res.json({ message: 'Teste mensagem' })  
+})
+
+mongoose.connect('mongodb+srv://testebanco:12345@apicluster.afkln.mongodb.net/bancoapirest?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Conectou ao banco')
+    app.listen(8000)
+})
+.catch((er) => console.log(er))
+
