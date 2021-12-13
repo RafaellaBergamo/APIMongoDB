2a63b491855abc2dd9443789e0982515c8d505a4
Rafaella <rafaella.bergamo@plasutil.com.br>
2021-12-13 11:38:12 -0300

    Criado rotas de buscar usuário e atualizar usuário por id

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -10,9 +10,11 @@ router.post('/', async (req, res) => {
 
     if(!name) {
         res.status(422).json({ error: 'O nome é obrigatório' })
+        return
     }
     if(!salary) {
         res.status(422).json({ error: 'O salário é obrigatório' })
+        return
     }
 
     const person = {
@@ -33,6 +35,43 @@ router.get('/', async (req, res) => {
     try {
         const people = await Person.find()
         res.status(200).json(people)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const person = await Person.findOne({ _id: id })
+
+        if(!person) {
+            res.status(422).json({ message: 'O usário não foi encontrado' })
+            return
+        }
+
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id
+    const {name, salary, approved} = req.body
+
+    const person = { name, salary, approved }
+
+    try {
+        const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+        if(updatedPerson.matchedCount === 0) {
+            res.status(422).json({ message: 'O usário não foi encontrado' })
+            return
+        }
+
+        res.status(200).json(person)
     } catch (error) {
         res.status(500).json({ error: error })
     }
